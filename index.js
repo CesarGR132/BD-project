3dcfd009350a7b610d484fbec8e48fb8c698870e
CesarGR132 <104606645+CesarGR132@users.noreply.github.com>
2025-01-31 19:11:08 -0600

    Command start added

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { PORT } from './config.js';
-import db from './db.js';
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import db from './db.js'
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 5000
 
-
-app.use(cors());
-app.use(bodyParser.json());
-
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/api/data1', (req, res) => {
-    db.query('SELECT * FROM bdpedidos', (err, rows) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            const result = rows.map(row => ({
-                id: row.idproducto,
-                value: row.cantidad,
-                label: row.producto
-            }));
-            db.end();
-            res.json(result);
-        }
-    });
+  db.query('SELECT * FROM bdpedidos', (err, rows) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      const result = rows.map(row => ({
+        id: row.idproducto,
+        value: row.cantidad,
+        label: row.producto
+      }))
+      db.end()
+      res.json(result)
+    }
+  })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
 })
-
